290f06a7608b2f9059dbcea7c5ce1b019f16bd93
mikong <michaelgalero@gmail.com>
2008-11-08 02:51:28 +0800

    Added currency_field helper.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -3,5 +3,9 @@ module Merb
     def currency_tip
       tag :span, '(in Php)', :class => 'tip'
     end
+    
+    def currency_field(name)
+      "#{text_field(name, :class => 'money')} .00"
+    end
   end
 end
